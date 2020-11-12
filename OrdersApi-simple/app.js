ef4c3fc6594fa4ebe0ef65ddd8b8a637dfcc5b33
Victor.Frts <victorfreitas.job@gmail.com>
2020-11-12 13:23:39 -0300

    Add Error handler for undefined routes  app.js

--- OrdersApi-simple/app.js
+++ OrdersApi-simple/app.js
@@ -3,9 +3,28 @@ const app = express();
 
 const productsRoute = require("./routes/products");
 const ordersRoute = require("./routes/orders");
+const morgan = require("morgan");
+
+app.use(morgan("dev"));
 
 app.use("/products", productsRoute);
 app.use("/orders", ordersRoute);
+
+//If no routes
+app.use((req, res, next) => {
+  const error = new Error("Route Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  return res.send({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 module.exports = app;
 
