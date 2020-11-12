f1768d2b7d5fb0e396e41ceac0ea8ff8b8fe7dd8
Victor.Frts <victorfreitas.job@gmail.com>
2020-11-12 13:47:33 -0300

    Add Cors Configuration manually app.js

--- OrdersApi-simple/app.js
+++ OrdersApi-simple/app.js
@@ -4,8 +4,27 @@ const app = express();
 const productsRoute = require("./routes/products");
 const ordersRoute = require("./routes/orders");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false })); //only simple data
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Acess-Control-Allow-Origin", "*");
+  res.header(
+    "Acess-Control-Allow-Header",
+    "Content-Type",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    res.header("Acess-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
+    return res.status(200).send({});
+  }
+
+  next();
+});
 
 app.use("/products", productsRoute);
 app.use("/orders", ordersRoute);
