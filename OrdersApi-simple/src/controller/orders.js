62d9b9e90ec42204fd4e917845c2630f4cba336d
Victor.Frts <victorfreitas.job@gmail.com>
2020-11-13 11:18:59 -0300

    Add connection with DB /controller orders

--- OrdersApi-simple/src/controller/orders.js
+++ OrdersApi-simple/src/controller/orders.js
@@ -1,34 +1,127 @@
 const express = require("express");
 const router = express.Router();
+const pool = require("../sql/_database");
 
-router.get("/", (req, res, next) => {
-  res.status(200).send({
-    mensagem: "GetAll orders",
+router.get("/", (req, resp, next) => {
+  const getAllQuery = `SELECT * FROM orders`;
+
+  pool.query(getAllQuery, (err, result) => {
+    if (err) return resultonse.status(500).send({ error: err });
+    const response = {
+      totalItems: result.rows.length,
+      orders: result.rows.map((order) => {
+        return {
+          id: order.id,
+          id_product: order.id_product,
+          qtt: order.qtt,
+          orderDate: order.order_date,
+          request: {
+            type: "GET",
+            url: "http:localhost:3000/orders/" + order.id,
+            description: "Return product by id",
+          },
+        };
+      }),
+    };
+
+    return resp.status(200).send({ response });
   });
 });
 
-router.get("/:id_order", (req, res, next) => {
-  const id = req.params.id_order;
-  res.status(200).send({
-    message: "get orders by id",
-    id: id,
+router.get("/:id", (req, resp, next) => {
+  const { id } = req.params;
+  const getByIdQuery = `SELECT * FROM orders WHERE id = $1`;
+  pool.query(getByIdQuery, [id], (err, result) => {
+    if (err) return err.status(500).send({ error: err });
+
+    if (!result.rowCount) {
+      return resp.status(404).send({
+        message: "Order not found for id: " + id,
+      });
+    }
+
+    const response = {
+      order: {
+        id: result.rows[0].id,
+        id_product: result.rows[0].id_product,
+        qtt: result.rows[0].qtt,
+        orderDate: result.rows[0].order_date,
+        request: {
+          type: "GET",
+          url: "http:localhost:3000/orders",
+          description: "Return all orders",
+        },
+      },
+    };
+
+    return resp.status(200).send(response);
   });
 });
 
-router.post("/", (req, res, next) => {
-  const order = {
-    id_product: req.body.id_product,
-    qtt: req.body.qtt,
-  };
-  res.status(201).send({
-    message: "Add product sucessfull!",
-    createdProduct: order,
+router.post("/", (req, resp, next) => {
+  const { id_product, qtt } = req.body;
+
+  const postQuery = `INSERT INTO orders 
+  (id_product, qtt)
+  VALUES ($1, $2)`;
+
+  pool.query(postQuery, [id_product, qtt], (err, result) => {
+    if (err) {
+      return resp.status(500).send({ error: err });
+    }
+
+    const response = {
+      message: "Order created sucessfully!",
+      createdOrder: {
+        id: result.id,
+        id_product,
+        qtt,
+        request: {
+          type: "GET",
+          url: "http:localhost:3000/orders",
+          description: "List all orders",
+        },
+      },
+    };
+
+    return resp.status(201).send({ response });
   });
 });
 
-router.delete("/", (req, res, next) => {
-  res.status(200).send({
-    mesagem: "DELETE",
+router.delete("/:id", (req, resp, next) => {
+  const { id } = req.params;
+  const deleteQuery = `DELETE FROM orders WHERE id= $1`;
+  const getByIdQuery = `SELECT * FROM orders WHERE id = $1`;
+
+  pool.query(getByIdQuery, [id], (err, result) => {
+    if (err) return err.status(500).send({ error: err });
+
+    if (!result.rowCount) {
+      return resp.status(404).send({
+        message: "Order not found for id: " + id,
+      });
+    }
+
+    pool.query(deleteQuery, [id], (err, result) => {
+      if (err) {
+        return resp.status(500).send({ error: err });
+      }
+
+      const response = {
+        message: "Product with id " + id + " deleted sucessfully!",
+        request: {
+          type: "POST",
+          url: "http:localhost:3000/orders",
+          description: "Create a brand new order",
+          body: {
+            id_product: "Number",
+            qtt: "Number",
+          },
+        },
+      };
+
+      return resp.status(202).send(response);
+    });
   });
 });
 
