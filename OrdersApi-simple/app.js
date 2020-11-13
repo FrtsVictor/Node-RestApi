bca65d9bab9eb799a4aeda3a088388eb41ced684
Victor.Frts <victorfreitas.job@gmail.com>
2020-11-12 23:37:08 -0300

    Simplify cors  *  app.js

--- OrdersApi-simple/app.js
+++ OrdersApi-simple/app.js
@@ -1,35 +1,21 @@
 const express = require("express");
 const app = express();
 
-const productsRoute = require("./routes/products");
-const ordersRoute = require("./routes/orders");
+const productsRoute = require("./src/controller/products");
+const ordersRoute = require("./src/controller/orders");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 
+var cors = require("cors");
+app.use(cors());
+
 app.use(morgan("dev"));
-app.use(bodyParser.urlencoded({ extended: false })); //only simple data
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.use((req, res, next) => {
-  res.header("Acess-Control-Allow-Origin", "*");
-  res.header(
-    "Acess-Control-Allow-Header",
-    "Content-Type",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-
-  if (req.method === "OPTIONS") {
-    res.header("Acess-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
-    return res.status(200).send({});
-  }
-
-  next();
-});
 
 app.use("/products", productsRoute);
 app.use("/orders", ordersRoute);
 
-//If no routes
 app.use((req, res, next) => {
   const error = new Error("Route Not Found");
   error.status = 404;
@@ -46,5 +32,3 @@ app.use((error, req, res, next) => {
 });
 
 module.exports = app;
-
-// nodemon --inspect ./server.js 80
