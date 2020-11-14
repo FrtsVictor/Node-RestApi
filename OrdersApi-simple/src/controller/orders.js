98a93f52ee0c3e332f61a35f1d2a505a5391e2b7
Victor.Frts <victorfreitas.job@gmail.com>
2020-11-14 10:00:31 -0300

    Fix code styles /controller orders

--- OrdersApi-simple/src/controller/orders.js
+++ OrdersApi-simple/src/controller/orders.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const pool = require("../sql/_database");
+const pool = require('../sql/_database');
 
-router.get("/", (req, resp, next) => {
+router.get('/', (req, resp, next) => {
   const getAllQuery = ` SELECT ord.id,
                                ord.qtt,
                                pdt.name,
@@ -13,41 +13,39 @@ router.get("/", (req, resp, next) => {
                             ON ord.id_product = pdt.id`;
 
   pool.query(getAllQuery, (err, result) => {
-    if (err) return resultonse.status(500).send({ error: err });
+    if (err) return resp.status(500).send({ error: err });
     const response = {
       totalItems: result.rows.length,
-      orders: result.rows.map((order) => {
-        return {
-          id: order.id,
-          qtt: order.qtt,
-          orderDate: order.order_date,
-          product: {
-            id_product: order.id_product,
-            product_name: order.name,
-            product_price: order.price,
-          },
-          request: {
-            type: "GET",
-            url: "http:localhost:3000/orders/" + order.id,
-            description: "Return product by id",
-          },
-        };
-      }),
+      orders: result.rows.map((order) => ({
+        id: order.id,
+        qtt: order.qtt,
+        orderDate: order.order_date,
+        product: {
+          id_product: order.id_product,
+          product_name: order.name,
+          product_price: order.price,
+        },
+        request: {
+          type: 'GET',
+          url: `http:localhost:3000/orders/${order.id}`,
+          description: 'Return product by id',
+        },
+      })),
     };
 
     return resp.status(200).send({ response });
   });
 });
 
-router.get("/:id", (req, resp, next) => {
+router.get('/:id', (req, resp, next) => {
   const { id } = req.params;
-  const getByIdQuery = `SELECT * FROM orders WHERE id = $1`;
+  const getByIdQuery = 'SELECT * FROM orders WHERE id = $1';
   pool.query(getByIdQuery, [id], (err, result) => {
     if (err) return err.status(500).send({ error: err });
 
     if (!result.rowCount) {
       return resp.status(404).send({
-        message: "Order not found for id: " + id,
+        message: `Order not found for id: ${id}`,
       });
     }
 
@@ -58,9 +56,9 @@ router.get("/:id", (req, resp, next) => {
         qtt: result.rows[0].qtt,
         orderDate: result.rows[0].order_date,
         request: {
-          type: "GET",
-          url: "http:localhost:3000/orders",
-          description: "Return all orders",
+          type: 'GET',
+          url: 'http:localhost:3000/orders',
+          description: 'Return all orders',
         },
       },
     };
@@ -69,13 +67,16 @@ router.get("/:id", (req, resp, next) => {
   });
 });
 
-router.post("/", (req, resp, next) => {
+router.post('/', (req, resp, next) => {
   const { id_product, qtt } = req.body;
-  const getByIdQuery = `SELECT * FROM products WHERE id = $1`;
-  const postQuery = `INSERT INTO orders 
-  (id_product, qtt)
-  VALUES ($1, $2)`;
-
+  const getByIdQuery = `
+            SELECT *
+              FROM products
+             WHERE id = $1`;
+  const postQuery = `
+            INSERT INTO orders
+              (id_product, qtt)
+            VALUES ($1, $2)`;
   pool.query(getByIdQuery, [id_product], (err, result) => {
     if (err) {
       return resp.status(500).send({ errorGet: err.stack });
@@ -83,7 +84,7 @@ router.post("/", (req, resp, next) => {
 
     if (!result.rowCount) {
       return resp.status(404).send({
-        message: "Order not found for id: " + id_product,
+        message: `Product not found for id: ${id_product}`,
       });
     }
 
@@ -93,15 +94,15 @@ router.post("/", (req, resp, next) => {
       }
 
       const response = {
-        message: "Order created sucessfully!",
+        message: 'Order created sucessfully!',
         createdOrder: {
           id: result.id,
           id_product,
           qtt,
           request: {
-            type: "GET",
-            url: "http:localhost:3000/orders",
-            description: "List all orders",
+            type: 'GET',
+            url: 'http:localhost:3000/orders',
+            description: 'List all orders',
           },
         },
       };
@@ -111,17 +112,21 @@ router.post("/", (req, resp, next) => {
   });
 });
 
-router.delete("/:id", (req, resp, next) => {
+router.delete('/:id', (req, resp, next) => {
   const { id } = req.params;
-  const deleteQuery = `DELETE FROM orders WHERE id= $1`;
-  const getByIdQuery = `SELECT * FROM orders WHERE id = $1`;
+  const deleteQuery = `
+          DELETE FROM orders
+          WHERE  id= $1`;
+  const getByIdQuery = `
+          SELECT * FROM orders
+          WHERE id = $1`;
 
   pool.query(getByIdQuery, [id], (err, result) => {
     if (err) return err.status(500).send({ error: err });
 
     if (!result.rowCount) {
       return resp.status(404).send({
-        message: "Order not found for id: " + id,
+        message: `Order not found for id: ${id}`,
       });
     }
 
@@ -131,14 +136,14 @@ router.delete("/:id", (req, resp, next) => {
       }
 
       const response = {
-        message: "Product with id " + id + " deleted sucessfully!",
+        message: `Product with id ${id} deleted sucessfully!`,
         request: {
-          type: "POST",
-          url: "http:localhost:3000/orders",
-          description: "Create a brand new order",
+          type: 'POST',
+          url: 'http:localhost:3000/orders',
+          description: 'Create a brand new order',
           body: {
-            id_product: "Number",
-            qtt: "Number",
+            id_product: 'Number',
+            qtt: 'Number',
           },
         },
       };
