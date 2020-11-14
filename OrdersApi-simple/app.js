a8b69faf49b9a9dc3b19fe4fa67c58cc7522bfa4
Victor.Frts <victorfreitas.job@gmail.com>
2020-11-14 12:49:02 -0300

    Add User Route  app.js

--- OrdersApi-simple/app.js
+++ OrdersApi-simple/app.js
@@ -1,22 +1,25 @@
-const express = require("express");
-const productsRoute = require("./src/controller/products");
-const ordersRoute = require("./src/controller/orders");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-var cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const productsRoute = require('./src/controller/products');
+const ordersRoute = require('./src/controller/orders');
+const usersRoute = require('./src/controller/users');
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(cors());
 
-app.use("/products", productsRoute);
-app.use("/orders", ordersRoute);
-app.use("/uploads", express.static("uploads")); //setting upload directory public
+app.use('/products', productsRoute);
+app.use('/orders', ordersRoute);
+app.use('/users', usersRoute);
+app.use('/uploads', express.static('uploads')); //setting upload directory public
 
 app.use((req, res, next) => {
-  const error = new Error("Route Not Found");
+  const error = new Error('Route Not Found');
   error.status = 404;
   next(error);
 });
