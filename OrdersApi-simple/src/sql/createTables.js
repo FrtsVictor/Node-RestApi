8720430d84e8ffe98c20b99a7b1c711a9fa6108f
Victor.Frts <victorfreitas.job@gmail.com>
2020-11-16 19:56:51 -0300

    Add createTable Images /sql createTables

--- OrdersApi-simple/src/sql/createTables.js
+++ OrdersApi-simple/src/sql/createTables.js
@@ -3,7 +3,7 @@ const db = require('./_database');
 const createTables = async () => {
   await db.connect();
 
-  await db.query(`create table products(
+  await db.query(`create table if not exists products(
         id serial primary key,
         name varchar(80) not null unique,
         price decimal (10,2) not null default 0,
@@ -12,19 +12,25 @@ const createTables = async () => {
         image varchar(500)
         );`);
 
-  await db.query(`create table orders (
+  await db.query(`create table if not exists orders (
     id serial primary key,
     id_product integer references products (id) on delete cascade not null,
     qtt integer not null,
     order_date date default current_date not null
     );`);
 
-  await db.query(`create table users (
+  await db.query(`create table if not exists users (
       id serial primary key,
       email varchar(100) not null unique,
       password varchar(200) not null,
       username varchar (50) not null unique
     )`);
+
+  await db.query(`create table if not exists products_img (
+      id serial primary key,
+      id_product integer references products (id) on delete cascade not null,
+      img_path VARCHAR (300)
+      )`);
 
   await db.end();
 
