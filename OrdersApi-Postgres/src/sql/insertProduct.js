5c20a47037c642e399a1658083b67ac31cf2975f
Victor.Frts <victorfreitas.job@gmail.com>
2020-11-18 16:37:02 -0300

    Refactor insertProducts /sql

--- OrdersApi-Postgres/src/sql/insertProduct.js
+++ OrdersApi-Postgres/src/sql/insertProduct.js
@@ -1,43 +1,21 @@
 const db = require('./_database');
 
-const insertTestData = async () => {
-  await db.connect();
-
-  const queryProduct =
-    'INSERT INTO products (name, price, qtt_stock, description) VALUES($1, $2, $3, $4)';
-
-  await db.query(queryProduct, [
-    'Xiami Mi8 Plus',
-    1500.9,
-    10,
-    'Xiami Mi8 Plus 120GB HD 6GB RAM',
-  ]);
-  await db.query(queryProduct, [
-    'Xiami Mi9 PlusC',
-    1700.9,
-    10,
-    'Xiami Mi9 PlusC 180GB HD 8GB RAM',
-  ]);
-  await db.query(queryProduct, [
-    'Xiami RedmeNote Pro',
-    1450.9,
-    10,
-    'Xiami RedmeNote Pro 220GB HD, 8GB RAM',
-  ]);
-  await db.query(queryProduct, [
-    'Xiaomi Pro Turbo',
-    3900.9,
-    10,
-    'Xiaomi Pro Turbo 220GB HD 10GB RAM',
-  ]);
-  await db.query(queryProduct, [
-    'Xiaomi Pocophone',
-    3500.9,
-    10,
-    'Xiaomi Pocophone 220GB HD 12GB RAM',
-  ]);
+const insertProducts = async () => {
+  console.log('test');
+  const queryProduct = `INSERT INTO products
+  ("name", price, qtt_stock, description, image, category_id)
+     VALUES
+     ('Xiaomi Mi8 Plus', 1500.9, 10, 'Xiaomi Mi8 Plus 120GB HD 6GB RAM', '', 3),
+     ('Xiaomi Mi9 PlusC', 1700.9, 10, 'Xiaomi Mi9 PlusC 180GB HD 8GB RAM', '', 3),
+     ('Xiaomi RedmeNote Pro', 1450.9, 10, 'Xiaomi Pro 220GB HD, 8GB RAM', '', 3),
+     ('World Of Warcraft', 150.00, 10, 'MMRO RPG TARGET', '', 2),
+     ('Tera', 160.00, 10, 'MMRO ACTION RPG', '', 2),
+     ('Black OPS', 120, 10, 'FPS Game', '', 1 ),
+     ('CODE', 100, 10, 'FPS Game CODE', '', 1);
+    `;
+  await db.execute(queryProduct);
 
   console.log('Insert products ok');
 };
 
-insertTestData();
+insertProducts();
