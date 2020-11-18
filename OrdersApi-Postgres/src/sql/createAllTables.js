a762474c7840b098fd7255ea7c089fa7ee0a7c43
Victor.Frts <victorfreitas.job@gmail.com>
2020-11-18 16:38:03 -0300

    Add createAllTables /sql

---
+++ OrdersApi-Postgres/src/sql/createAllTables.js
@@ -0,0 +1,54 @@
+const db = require('./_database');
+
+const createTables = async () => {
+  await db.execute(`create table if not exists products(
+        id serial primary key,
+        name varchar(80) not null unique,
+        price decimal (10,2) not null default 0,
+        qtt_stock int not null default 0,
+        description varchar(80) not null,
+        image varchar(500)
+        );`);
+  console.log('Created table products');
+
+  await db.execute(`create table if not exists orders (
+    id serial primary key,
+    id_product integer references products (id) on delete cascade not null,
+    qtt integer not null,
+    order_date date default current_date not null
+    );`);
+  console.log('Created table orders');
+
+  await db.execute(`create table if not exists users (
+      id serial primary key,
+      email varchar(100) not null unique,
+      password varchar(200) not null,
+      username varchar (50) not null unique
+    )`);
+
+  await db.execute(`create table if not exists products_img (
+      id serial primary key,
+      id_product integer references products (id) on delete cascade not null,
+      img_path VARCHAR (300)
+      )`);
+
+  await db.execute(`create table if not exists categories(
+        id serial PRIMARY key,
+        name varchar(100) not null unique,
+        description varchar(150) not null
+    )`);
+
+  await db.execute('alter table products add column category_id int not null');
+
+  await db.execute(
+    'alter table products alter column category_id set not null'
+  );
+
+  await db.execute(
+    'alter table products add constraint fk_product_category foreign key (category_id) references categories (id)'
+  );
+
+  console.log('Created All tables sucessfully');
+};
+
+createTables();
