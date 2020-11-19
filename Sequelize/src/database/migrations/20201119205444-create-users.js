7dcf65628cb2092e573d20d2b82030b86a5ee937
Victor.Frts <victorfreitas.job@gmail.com>
2020-11-19 20:02:25 -0300

    Add migration CreateUsersTable /database/migrations

---
+++ Sequelize/src/database/migrations/20201119205444-create-users.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const sequelize = require("sequelize");
+
+module.exports = {
+  //up o que vai fazer no DB
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("users", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  //down se der problema o que fazço?
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("users");
+  },
+};
