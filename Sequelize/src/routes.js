c8a684680f733077f342f95ade5cc5fd11486714
Victor.Frts <victorfreitas.job@gmail.com>
2020-11-19 19:57:48 -0300

    Add getAll & post routes users & adresses /root routes

--- Sequelize/src/routes.js
+++ Sequelize/src/routes.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const UserController = require("./controllers/UserController");
+const AddressesController = require("./controllers/AdressesController");
 
 const routes = express.Router();
 
-routes.get("/", (req, resp) => {
-  return resp.json({ hello: "World" });
-});
+routes.get("/users", UserController.index);
+routes.post("/users", UserController.Store);
+
+routes.get("/users/:user_id/addresses", AddressesController.index);
+routes.post("/users/:user_id/addresses", AddressesController.Store);
 
 module.exports = routes;
