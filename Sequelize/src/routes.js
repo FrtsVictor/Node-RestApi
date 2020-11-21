ebe0f41ed24d6188cacb6fbc514fd7cebe4e6868
Victor.Frts <victorfreitas.job@gmail.com>
2020-11-20 22:30:22 -0300

    Add route Get&Post  Tech /root

--- Sequelize/src/routes.js
+++ Sequelize/src/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const UserController = require("./controllers/UserController");
 const AdressController = require("./controllers/AdressController");
+const TechController = require("./controllers/TechController");
 
 const routes = express.Router();
 
@@ -9,5 +10,8 @@ routes.post("/users", UserController.Store);
 
 routes.get("/users/:user_id/addresses", AdressController.index);
 routes.post("/users/:user_id/addresses", AdressController.Store);
+
+routes.get("/users/:user_id/techs", TechController.index);
+routes.post("/users/:user_id/techs", TechController.Store);
 
 module.exports = routes;
