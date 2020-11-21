d39fcd7115ec9a69c3a5cfed0481586a795c95c0
Victor.Frts <victorfreitas.job@gmail.com>
2020-11-21 16:29:18 -0300

    Add routes  /root

---
+++ SOLID-Typescript/src/routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/users", (request, response) => {
+  return response.status(201).send({ hello: "wtf" });
+});
+
+router.post("/users", (request, response) => {
+  return response.status(201).send();
+});
+
+export { router };
